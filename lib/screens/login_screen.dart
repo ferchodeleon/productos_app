440f8653fe9d5c14222eab5616c4010d48835f45
Fernando De Leon <gasnare@hotmail.com>
2021-07-28 17:36:40 -0500

    Add the widget login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,7 +7,13 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AuthBackground(),
+      body: AuthBackground(
+        child: Container(
+          width: double.infinity,
+          height: 300,
+          color: Colors.red,
+        ),
+      ),
     );
   }
 }

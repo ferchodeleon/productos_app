ef94aa0fab7939f9c819f54a898ee8a9f5685c07
Fernando De Leon <gasnare@hotmail.com>
2021-09-09 19:14:54 -0500

    Add route to product screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,7 +14,10 @@ class HomeScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemCount: 10,
-        itemBuilder: (BuildContext context, int index) => ProductCard(),
+        itemBuilder: (BuildContext context, int index) => GestureDetector(
+          onTap: () => Navigator.pushNamed(context, 'productScreen'),
+          child: ProductCard(),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},

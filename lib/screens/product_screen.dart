7eface42cc80873881eab248e2ae155e9566fd69
Fernando De León Gil <gasnare@hotmail.com>
2021-09-12 13:33:16 -0500

    ÂrcOCereate to the form view product

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
 
 ///Screen product details
@@ -6,28 +7,108 @@ class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              ProductImage(),
-              Positioned(
-                top: 60,
-                left: 40,
-                child: IconButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  icon: const Icon(
-                    Icons.arrow_back_ios,
-                    size: 40,
-                    color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 40,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      size: 40,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
+                Positioned(
+                  top: 60,
+                  right: 40,
+                  child: IconButton(
+                    onPressed: () {
+                      //TODO: Request Camera or gallery
+                    },
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            _ProductForm(),
+            const SizedBox(height: 100),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save_outlined),
+      ),
+    );
+  }
+}
+
+class _ProductForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre del producto',
+                  labelText: 'Nombre',
+                ),
               ),
+              const SizedBox(height: 30),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150',
+                  labelText: 'Precio:',
+                ),
+              ),
+              const SizedBox(height: 30),
+              SwitchListTile.adaptive(
+                value: true,
+                title: Text('Disponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 30),
             ],
           ),
-        ],
+        ),
       ),
-    ));
+    );
   }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: const Offset(0, 5),
+            blurRadius: 5,
+          )
+        ],
+      );
 }

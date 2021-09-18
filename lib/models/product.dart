1324249eb48f5beac44d21866cd003b060873f80
Fernando De León Gil <gasnare@hotmail.com>
2021-09-18 18:29:22 -0500

    Create model product return to backend firebase

---
+++ lib/models/product.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+// ignore: public_member_api_docs
+class Product {
+  // ignore: public_member_api_docs
+  Product({
+    required this.available,
+    required this.name,
+    this.picture,
+    required this.price,
+  });
+
+  // ignore: public_member_api_docs
+  bool available;
+  // ignore: public_member_api_docs
+  String name;
+  // ignore: public_member_api_docs
+  String? picture;
+  // ignore: public_member_api_docs
+  double price;
+
+  factory Product.fromJson(String str) =>
+      Product.fromMap(json.decode(str) as Map<String, dynamic>);
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromMap(Map<String, dynamic> json) => Product(
+        available: json['available'] as bool,
+        name: json['name'] as String,
+        picture: json['picture'] as String,
+        price: json['price'] as double,
+      );
+
+  // ignore: public_member_api_docs
+  Map<String, dynamic> toMap() => {
+        // ignore: prefer_single_quotes
+        "available": available,
+        // ignore: prefer_single_quotes
+        "name": name,
+        // ignore: prefer_single_quotes
+        "picture": picture,
+        // ignore: prefer_single_quotes
+        "price": price,
+      };
+}
